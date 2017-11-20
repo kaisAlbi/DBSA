f62dafeb2277148ccb14781b5333d0522bd66e7c
TanguyH <tanguyh@icloud.com>
2017-11-20 15:37:15 +0100

    OutputStream update
    Finished structure of OutputStream

--- OutputStream.cpp
+++ OutputStream.cpp
@@ -9,7 +9,72 @@
 #include <cstdint>
 
 class OutputStream {
+    int _used_method;
+    
+    void _write1(uint32_t);
+    void _write2(uint32_t);
+    void _write3(uint32_t);
+    void _write4(uint32_t);
 
 public:
-    OutputStream();
+    // getters
+    int getUsedMethod();
+    
+    // setters
+    void setUsedMethod(int);
+    
+    // required functions
+    void create(std::string);
+    void write(uint32_t);
+    void close();
+};
+// getters
+int OutputStream::getUsedMethod() {
+    return _used_method;
 }
+
+// setters
+void OutputStream::setUsedMethod(int new_method) {
+    this->_used_method = (new_method % 4) + 1;
+}
+
+// required functions
+void OutputStream::create(std::string file_name) {
+    std::cout << "creating file withe file name : " << file_name << std::endl;
+}
+
+void OutputStream::_write1(uint32_t value) {
+}
+
+void OutputStream::_write2(uint32_t value) {
+}
+
+void OutputStream::_write3(uint32_t value) {
+}
+
+void OutputStream::_write4(uint32_t value) {
+}
+
+void OutputStream::write(uint32_t value) {
+    std::cout << "writing value " << value << " to the stream" << std::endl;
+    
+    switch (this->getUsedMethod()) {
+        case 1:
+            this->_write1(value);
+            break;
+        case 2:
+            this->_write2(value);
+            break;
+        case 3:
+            this->_write3(value);
+            break;
+        case 4:
+            this->_write4(value);
+            break;
+    }
+}
+
+void OutputStream::close() {
+    std::cout << "closing stream" << std::endl;
+}
+
