8fa349e5063473c058816a3db2f5f9e32eb969fb
NBaudoux <nbaudoux@ulb.ac.be>
2017-11-20 16:47:29 +0100

    Correction to generate signed value

--- diskFiles/fileGen.cpp
+++ diskFiles/fileGen.cpp
@@ -23,9 +23,13 @@ public:
 		std::random_device rd;     	//Get a random seed from the OS entropy device, or whatever
 		std::mt19937 eng(rd()); 	//Use the 32-bit Mersenne Twister 19937 generator
 									//and seed it with entropy.
+									
+		int32_t int32min = std::numeric_limits<int32_t>::min(); //minimum value
+		int32_t int32max = std::numeric_limits<int32_t>::max(); //maximum value 
+		
 		//Define the distribution, by default it goes from 0 to MAX(unsigned long long)
 		//or what have you. 
-		std::uniform_int_distribution<uint32_t> distr;
+		std::uniform_int_distribution<int32_t> distr(int32min,int32max);
 		for(std::size_t i = 0; i < _length; ++i){
 			//Writing in file
 			outputFile << distr(eng);
@@ -35,6 +39,7 @@ public:
 	}	
 };
 
+//Test
 int main(){
 	fileGen(10, "test.txt");
 }
